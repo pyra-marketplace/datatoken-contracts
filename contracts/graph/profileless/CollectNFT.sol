fc4dd5ea7c29091746bdaede4fc57d74ebbfa9cb
CanvasL <746591811@qq.com>
2023-11-11 12:10:17 +0800

    chore: slither config

--- contracts/graph/profileless/CollectNFT.sol
+++ contracts/graph/profileless/CollectNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.10;
 
 import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
